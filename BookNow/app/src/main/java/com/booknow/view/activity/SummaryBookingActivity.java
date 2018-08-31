e5ddddd6465c1573395a30be986aa2249947cb13
nick_camilli <nalcaine@everis.com>
2018-08-31 09:34:02 +0200

    Actualizado SummaryBooking

---
+++ BookNow/app/src/main/java/com/booknow/view/activity/SummaryBookingActivity.java
@@ -0,0 +1,18 @@
+package com.booknow.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SummaryBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_booking);
+    }
+
+    public void onDeleteBookingClick(View view){
+
+    }
+}
